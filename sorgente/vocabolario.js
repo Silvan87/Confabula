d8852f31d86bbee3ce61fce637e0413025985117
Silvan87 <silvano87@yahoo.it>
2017-04-22 15:14:53 +0200

    Corretta gestione delle parole omesse
    Semplificata la funzione che cerca una corrispondenza tra una frase normalizzata in un insieme di frasi normalizzate ed al tempo stesso corretto il suo funzionamento.

--- sorgente/vocabolario.js
+++ sorgente/vocabolario.js
@@ -2,12 +2,16 @@ function vocabolario() {
 	Lingua.predicati("guardo|osservo|esamino|prendo|inventario|apro|chiudo|entro|esco|leggo");
 	Lingua.equivalenze([
 		["ovest|o"], ["nord|n"], ["est|e"], ["sud|s"],
-		["|'"], ["|il|lo|la|i|gli|le|l|gl"], ["|un|uno|una"],
-		["|di|del|dello|della|dei|degli|delle|dell|d"],
-		["|a|al|allo|alla|ai|agli|alle|all"],
-		["|da|dal|dallo|dalla|dai|dagli|dalle|dall"],
-		["|in|nel|nello|nella|nei|negli|nelle|nell"],
-		["|su|sul|sullo|sulla|sui|sugli|sulle|sull"],
+		["|il|lo|la|i|gli|le|l|gl"], ["|un|uno|una"],
+		["di|del|dello|della|dei|degli|delle|dell|d"],
+		["a|al|allo|alla|ai|agli|alle|all"],
+		["da|dal|dallo|dalla|dai|dagli|dalle|dall"],
+		["in|nel|nello|nella|nei|negli|nelle|nell"],
+		["con|col|collo|colla|coi|congli|colle|coll"],
+		["su|sul|sullo|sulla|sui|sugli|sulle|sull"],
+		["per|pel|perlo|perla|pei|pergli|perle|perl"],
+		["tra|tral|trallo|tralla|trai|tragli|tralle|tral"],
+		["fra|fral|frallo|fralla|frai|fragli|fralle|fral"],
 		["|esamino|esamina"], ["guardo|guarda"], ["osservo|osserva"], ["prendo|prendi"], ["apro|apri"], ["chiudo|chiudi"], ["entro|entra"], ["esco|esci"], ["leggo|leggi"]
 	]);
 }
